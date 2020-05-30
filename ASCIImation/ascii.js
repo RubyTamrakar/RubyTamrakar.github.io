0d783442b478a5e84682e013acb435446ba5b143
Ruby <rubytamrakar077@gmail.com>
2020-05-30 12:34:00 -0500

    validators

--- ASCIImation/ascii.js
+++ ASCIImation/ascii.js
@@ -1,38 +1,88 @@
-    var startB = document.getElementById("startButton");
-    startB.addEventListener("click", startEventHandler);
+"use strict";
+
+let index = 0;
+let currentAnimation;
+let timer = null;
+let startAnim;
+let delay = 250;
+let startB = document.getElementById("startButton");
+startB.addEventListener("click", startEventHandler);
+
+function startEventHandler() {
+    toggleController('start');
+    startAnim = document.getElementById("myTextArea");
+    currentAnimation = startAnim.value;
+    timer = setInterval(displayPattern, delay);
+}
+
+function displayPattern() {
+
+    let frames = currentAnimation.split("=====\n");
+    // console.log(frames);
+    startAnim.value = frames[index];
+    index++;
+    if (index === frames.length)
+        index = 0;
+}
+
+var stopB = document.getElementById("stopButton");
+stopB.addEventListener("click", stopEventHandler);
+
+function stopEventHandler() {
+    toggleController('stop');
+    clearInterval(timer);
+    startAnim.value = ANIMATIONS[animButton.value];
+}
+
+var animButton = document.getElementById("animType");
+animButton.addEventListener("change", animEventHandler);
+
+function animEventHandler() {
+
+    document.getElementById("myTextArea").value = ANIMATIONS[animButton.value];
+}
+
+// var sizeButton = document.getElementById("animSize");
+// sizeButton.addEventListener("change", sizeEventHandler);
+let animationSize = document.getElementById("animSize");
+animationSize.addEventListener("change", sizeEventHandler);
+
+function sizeEventHandler() {
+    // let sizeArray = ["tiny", "small", "medium", "large", "xl", "xxl"];
+    // sizeArray.forEach(element => {
+    //     if (animationSize.classList.contains(element)) {
+    //         animationSize.classList.remove(element);
+    //     }
+    // })
+    // animationSize.classList.add(animationSize.value);
+    var newSize = document.getElementById("animSize").value;
+    document.getElementById("myTextArea").style.fontSize = newSize;
+}
 
 
-    function startEventHandler() {
-        document.getElementById("myTextArea").value = "you pressed start button";
+var speedButton = document.getElementById("animSpeed");
+speedButton.addEventListener("change", speedEventHandler);
+
+function speedEventHandler() {
+    if (speedButton.checked) {
+        delay = 50;
+        clearInterval(timer)
+        timer = setInterval(displayPattern, delay);
+    } else {
+        delay = 250;
+        clearInterval(timer);
+        timer = setInterval(displayPattern, delay);
     }
+}
 
-    var stopB = document.getElementById("stopButton");
-    stopB.addEventListener("click", stopEventHandler);
-
-    function stopEventHandler() {
-        document.getElementById("myTextArea").value = "you pressed stop button";
+function toggleController(state) {
+    if (state == 'start') {
+        startButton.disabled = true;
+        stopButton.disabled = false;
+        animButton.disabled = true;
+    } else if (state == 'stop') {
+        startButton.disabled = false;
+        stopButton.disabled = true;
+        animButton.disabled = false;
     }
-
-    var animButton = document.getElementById("animType");
-    animButton.addEventListener("change", animEventHandler);
-
-    function animEventHandler() {
-
-        document.getElementById("myTextArea").value = ANIMATIONS[animButton.value];
-    }
-
-    var sizeButton = document.getElementById("animSize");
-    sizeButton.addEventListener("change", sizeEventHandler);
-
-    function sizeEventHandler() {
-        if (document.getElementById("myTextArea").value = "tiny") {
-
-        }
-    }
-
-    var speedButton = document.getElementById("animSpeed");
-    speedButton.addEventListener("change", speedEventHandler);
-
-    function speedEventHandler() {
-        document.getElementById("myTextArea").value = speedButton.checked;
-    }
+}
